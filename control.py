90f29ae1907f816e02989bdd1ab3b92beb7f1890
Yuxi Chang <michaelchangyx@g.ucla.edu>
2022-06-27 18:12:31 -0700

    Refactor detect code

--- control.py
+++ control.py
@@ -5,40 +5,49 @@ from lobe import ImageModel
 
 model = ImageModel.load('./waste_classifier')
 
-#define LED pins
-led = LED(17)
-motor_control = LED(27)
-empty_count = 0
-non_compostable_count = 0
+class Img_detect:
+    def __init__(self, led_pin = 17, motor_c_pin = 27):
+        self.led = LED(led_pin)
+        self.motor_control = LED(motor_c_pin)
+        self.empty_count = 0
+        self.non_compostable_count = 0
 
 
-def take_photo():
-    cam = cv2.VideoCapture(0)
-    ret, image = cam.read()
-    cv2.imshow('Imagetest',image)
-    cv2.imwrite('./img.jpg', image)
-    cam.release()
+    def take_photo(self):
+        cam = cv2.VideoCapture(0)
+        ret, image = cam.read()
+        cv2.imshow('Imagetest',image)
+        cv2.imwrite('./img.jpg', image)
+        cam.release()
 
-def control(label):
-    print(label)
-    if label == 'N' or label == 'R' or label == 'O':
-        empty_count = 0
-        if label != 'O':
-            non_compostable_count+=1
-            led.on() #non compostable detected
+    def control(self, label):
+        print(label)
+        if label == 'N' or label == 'R' or label == 'O':
+            self.empty_count = 0
+            if label != 'O':
+                self.non_compostable_count+=1
+                self.led.on() #non compostable detected
+            else:
+                self.non_compostable_count = 0
         else:
-            non_compostable_count = 0
-    else:
-        empty_count+=1
-    if empty_count > 5 or non_compostable_count > 2:
-        non_compostable_count = 0 #reset since user cleared the chamber
-        empty_count = 6 #cap the empty count to prevent overflow 
-        motor_control.off()
-    else:
-        motor_control.on()
+            self.empty_count+=1
+        if self.empty_count > 5 or self.non_compostable_count > 2:
+            self.non_compostable_count = 0 #reset since user cleared the chamber
+            self.empty_count = 6 #cap the empty count to prevent overflow 
+            self.motor_control.off()
+        else:
+            self.motor_control.on()
 
-while True:
-    take_photo()
-    result = model.predict_from_file('./img.jpg')
-    control(result.prediction)
-    sleep(1)
+    def run(self):
+        while True:
+            self.take_photo()
+            result = self.model.predict_from_file('./img.jpg')
+            self.control(result.prediction)
+            sleep(1)
+
+def main():
+    img_detect = Img_detect()
+    img_detect.run()
+
+if __name__ == "__main__":
+    main()
